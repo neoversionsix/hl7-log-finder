dc44ef370fa51bb3755af88c7dbc6d92c834aef0
Wason1 <jason.j.whittle@gmail.com>
2021-07-06 09:48:30 +1000

    up

---
+++ hl7-ultra.py
@@ -0,0 +1,24 @@
+#not working for use yet
+
+import os
+
+path = r'\\whsrhaparch1\RhapsodyHL7FileLogs_Prod\MasterLog'
+os.chdir(path)
+files = os.listdir()
+folders = list(filter(lambda x: not x.endswith('zip'), files))
+
+searchterm = '999999999'
+
+for afolder in folders:
+    path2 = os.path.join(path, afolder)
+    files2 = os.listdir(path2)
+    for afile in files2:
+        filepath = os.path.join(path2, afile)
+        with open(filepath, 'r') as f:
+            read_data = f.read()
+            hl7_list = read_data.split('======')
+            for hl7message in hl7_list:
+                if searchterm in hl7message:
+                    print(filepath)
+                    print(hl7message)
+                    print('')
